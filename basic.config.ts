f604e122cee5adfb56e03b48c48c63f73582e490
atooln <atulnair11@gmail.com>
2025-02-22 11:40:07 -0800

    adding config files

--- basic.config.ts
+++ basic.config.ts
@@ -1,18 +1,41 @@
 // basic.config.ts lets you define the schema for your database
 // after updating this file, you may need to restart the dev server
-// docs: https://docs.basic.tech/info/schema 
+// docs: https://docs.basic.tech/info/schema
 
 export const schema = {
-  project_id: '',
+  project_id: "study-buddies",
   version: 0,
   tables: {
-    emojis: {
-      type: 'collection',
+    users: {
+      type: "collection",
       fields: {
-        value: {
-          type: 'string',
+        email: {
+          type: "string",
+        },
+        name: {
+          type: "string",
+        },
+      },
+    },
+    notebooks: {
+      type: "collection",
+      fields: {
+        title: {
+          type: "string",
+        },
+        content: {
+          type: "string",
+        },
+        userId: {
+          type: "string",
+        },
+        createdAt: {
+          type: "number",
+        },
+        updatedAt: {
+          type: "number",
         },
       },
     },
   },
-}
+};
