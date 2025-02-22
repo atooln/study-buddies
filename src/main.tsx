cc3e25ac941dae3f0be07ab5a185f586de18134b
atooln <atulnair11@gmail.com>
2025-02-22 11:43:50 -0800

    modified main and app scripts to work with current configurations

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from "react-router";
-import { BasicProvider } from '@basictech/react';
-import { schema } from '../basic.config';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { BasicProvider } from "@basictech/react";
+import { schema } from "../basic.config";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BasicProvider project_id={schema.project_id} schema={schema}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />} />
-          <Route path="*" element={<div>404 - Page Not Found</div>} />
-        </Routes>
-      </BrowserRouter>
+      <App />
     </BasicProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
